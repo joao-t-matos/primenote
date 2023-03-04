994718ccb1e1c609b7229da4f6c4843fee81791d
João Matos <101014811+joao-t-matos@users.noreply.github.com>
2023-03-04 20:42:39 +0000

    6 add navbar (#7)
    * 6 add burger menu
    
    * 6 add menu icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import React, {FC, useState} from 'react';
 import './App.css';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './components/Menu'; 
 
-enum navigationScreens {
+export enum NavigationScreens {
   HOME = "HOME",
   ABOUT = "ABOUT"
 }
 
 const App: FC = () => {
-  const [navigation, setNavigation] = useState<String>();
+  const [navigation, setNavigation] = useState<string>(NavigationScreens.HOME);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const navigateHandler = (screenName: NavigationScreens) => {
+    setNavigation(screenName);
+  };
+  const toggleMenu = (value: boolean) => {
+    setIsMenuOpen(value);
+  };
+  
   return (
     <div className="App">
-      <Home/>
+      <button className="menu-button"
+      onClick={() => {toggleMenu(true)}}><DensityMediumIcon/></button>
+      {
+         isMenuOpen && (
+          <Menu 
+          toggleMenu={toggleMenu}
+          navigateHandler={navigateHandler}
+          />
+        )
+      }
+      {
+        navigation === NavigationScreens.HOME && (
+          <Home/>
+        )
+      }
+      {
+        navigation === NavigationScreens.ABOUT && (
+          <About/>
+        )
+      }
     </div>
   );
 }
