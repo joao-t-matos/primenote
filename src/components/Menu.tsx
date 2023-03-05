389971bdce8c41243c560fc6a0d0069dd2599337
joao-t-matos <j.t.s.m.fp@gmail.com>
2023-03-05 18:31:12 +0000

    5 add styling to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,9 @@
 import { Switch } from "@mui/material";
 import React, {FC} from "react";
 import { NavigationScreens } from "../App";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import ClearIcon from '@mui/icons-material/Clear';
 
 interface MenuProps {
   toggleMenu: (value: boolean) => void ;
@@ -10,16 +13,18 @@ interface MenuProps {
 };
 
 const Menu: FC<MenuProps> = ({toggleMenu, navigateHandler, toggleDarkMode, darkMode}) => {
-
-  const placeholder = "placeholder"
   return (
       <div className={`menu ${darkMode? 'menu-dark': 'menu-light'}`} >
-      <div className="menu-button-container">
-        <button  className="menu-close" onClick={() => {toggleMenu(false)}}>X</button>
-      </div>
-      <button onClick={() => {navigateHandler(NavigationScreens.HOME)}}>Home</button>
-      <button onClick={() => {navigateHandler(NavigationScreens.ABOUT)}}>About</button>
-      <Switch defaultChecked onChange={() => {toggleDarkMode()}}color="secondary" />
+        <div className="menu-button-container">
+          <button  className="menu-close" onClick={() => {toggleMenu(false)}}><ClearIcon fontSize="inherit"/></button>
+        </div>
+        <button className="menu-row" onClick={() => {navigateHandler(NavigationScreens.HOME)}}>Home</button>
+        <button className="menu-row" onClick={() => {navigateHandler(NavigationScreens.ABOUT)}}>About</button>
+        <div className="dark-mode-switch menu-row">
+          <WbSunnyIcon/>
+          <Switch defaultChecked onChange={() => {toggleDarkMode()}}color="secondary" />
+          <DarkModeIcon/>
+        </div>
       </div>
   )
 };
