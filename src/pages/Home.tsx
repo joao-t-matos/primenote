fe27f1ea837fbbcdfb99c6b588f99f7408ffa429
João Matos <101014811+joao-t-matos@users.noreply.github.com>
2023-04-05 15:27:39 +0100

    13 add text areas to home page (#23)
    * 13 add text areas to home page
    
    * Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,70 +39,20 @@ const Home: FC<HomeProps> = ({ darkMode }) => {
           minWidth: '100%',
           height: '100vh',
           backgroundColor: darkMode ? 'rgb(37, 37, 37)' : 'white',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
-        <CssBaseline />
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: darkMode ? 'rgb(37, 37, 37)' : 'white',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Home
-          </Typography>
-          <Box component="form" noValidate>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        <div className={`markdown-area ${darkMode? 'markdown-area-dark': 'markdown-area-light'}`}>
+          <div className="input-area">
+            <textarea placeholder='Input here ...'></textarea>
+          </div>
+          <div className="result-area">
+            <textarea className="result-output" disabled value={"output"}></textarea>
+            <textarea className="result-preview" disabled value={"preview"}></textarea>
+          </div>
+        </div>
       </Container>
     </>
   );
